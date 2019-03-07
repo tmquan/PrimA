9670a26bc1ca3816c43b1263d806d86e2f06e634
Sen <lotus@cinnamon.is>
2019-03-07 14:01:57 +0700

    Boilerplate

---
+++ Main.py
@@ -0,0 +1,41 @@
+# System modules
+import os
+import sys
+import glob
+import argparse
+import numpy as np
+from natsort import natsorted
+
+# Image processing, vision models
+import cv2
+
+# Machine learning modules
+from tensorpack import * 
+
+# Customize required modules to a specific model
+# from Dataflows import *
+# from Losses import *
+# from Models import *
+class ImageDataFlow(RNGDataFlow):
+	pass
+
+class Model(ModelDesc):
+	pass
+	
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+
+	# Parsing the argument
+	parser.add_argument('--gpu', help='comma seperated list of GPU(s) to use.',
+								 default='0')
+	parser.add_argument('--data',  default='data/Kasthuri15/3D/', required=True, 
+                                   help='Data directory, contain trainA/trainB/validA/validB for training')
+    parser.add_argument('--load',  help='Load the model path')
+    parser.add_argument('--dimx',  type=int, default=512)
+    parser.add_argument('--dimy',  type=int, default=512)
+    parser.add_argument('--dimz',  type=int, default=1)
+    parser.add_argument('--sample', help='Run the deployment on an instance',
+                                    action='store_true')
+
+    global args
+    args = parser.parse_args()
